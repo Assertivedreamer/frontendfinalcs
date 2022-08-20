b23ab0185fe9a6db965d0797a1b362c8f3c440b0
Shohrab Rustam <mohdrustam001@gmail.com>
2022-08-20 21:42:48 +0530

    table

--- src/app/table1/table1.component.spec.ts
+++ src/app/table1/table1.component.spec.ts
@@ -1,50 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
 
 import { Table1Component } from './table1.component';
-
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
 
 describe('Table1Component', () => {
   let component: Table1Component;
